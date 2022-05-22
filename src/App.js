567af1ecb8ed63c497d7977cb4d51144e8f3505c
ibktommy <tomquartz@gmail.com>
2022-05-22 11:01:55 +0100

    Mapping through the image-state array so as to render ImagesCard component for each image

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import ImagesCard from "./components/ImagesCard";
 
 function App() {
 	// Defining App Level State
@@ -24,35 +25,13 @@ function App() {
 	});
 
 	return (
-		<div className="max-w-sm rounded overflow-hidden shadow-lg">
-			<img src="https://source.unsplash.com/random" alt="" className="w-full" />
-			<div className="px-6 py-4">
-				<div className="font-bold text-pink-500 text-xl mb-2">Photo by John Doe</div>
-				<ul>
-					<li>
-						<strong>Views: </strong>
-						4000
-					</li>
-					<li>
-						<strong>Downloads: </strong>
-						300
-					</li>
-					<li>
-						<strong>Likes: </strong>
-						400
-					</li>
-				</ul>
-			</div>
-			<div className="px-6 py-4">
-				<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-					#tag1
-				</span>
-				<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-					#tag2
-				</span>
-				<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-					#tag3
-				</span>
+		<div className="container mx-auto">
+			<div className="grid grid-cols-3 gap-4">
+				{/* Display ImageCard Component as we map through images-state array */}
+
+				{images.map((image) => (
+					<ImagesCard key={image.id} image={image} />
+				))}
 			</div>
 		</div>
 	);
