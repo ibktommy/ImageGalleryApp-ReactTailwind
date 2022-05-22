10313607a1e418adcdcf744f8e982ce912ccf873
ibktommy <tomquartz@gmail.com>
2022-05-22 12:16:30 +0100

    Setting state on the SearchButtonComponent and passing in a funtion on the onChange event of the search input field

--- src/components/SearchButton.js
+++ src/components/SearchButton.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchButton = () => {
+	// Defining Seach Button Component State
+	const [input, setInput] = useState("");
+
 	return (
 		<div className="max-w-sm rounded overflow-hidden my-10 mx-auto">
 			<form className="w-full max-w-sm">
 				<div className="flex items-center border-b-2 border-teal-500 py-2">
 					<input
+						onChange={(e) => {
+							setInput(e.target.value);
+						}}
 						type="search"
 						placeholder="Search Image Term"
 						className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
 					/>
-					<button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 text-sm border-4 text-white py-1 px-2 rounded">
+					<button
+						type="submit"
+						className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+					>
 						Search
 					</button>
 				</div>
